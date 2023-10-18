6992351c5235c34f45e67774daac71f28118d585
LAVjr97 <lavjr97@gmail.com>
2023-10-18 16:31:23 -0700

    finished 146 lab4

--- coen146/lab4/webserver.py
+++ coen146/lab4/webserver.py
@@ -10,7 +10,6 @@
 
 from socket import *  # Import socket module
 import sys            # To terminate the program
-import os
 
 if __name__ == "__main__":
 
@@ -19,34 +18,37 @@ if __name__ == "__main__":
         print ('Usage: python %s <PortNumber>' % (sys.argv[0]))
         sys.exit()
     
-    socket_fd = socket(AF_INET, SOCK_STREAM)
-    socket_fd.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    socket_fd.bind(('127.0.0.1', int(sys.argv[1])))
-    socket_fd.listen(1)
+    socket_fd = socket(AF_INET, SOCK_STREAM) 
+    socket_fd.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) 
+    socket_fd.bind(('127.0.0.1', int(sys.argv[1]))) 
+    socket_fd.listen(1) 
 
     # Server should be up and running and listening to the incoming connections
     while True:
         print('The server is ready to respond to incoming requests...')
 
         connection_fd, addr = socket_fd.accept() 
-        byteBuff = connection_fd.recv(1024)
-        buff = str(byteBuff, 'utf-8')
+        byteBuff = connection_fd.recv(1024) 
+        buff = str(byteBuff, 'utf-8') #converts the binary recieved into a string 
+        print('\nRecieved HTTP request: \n' + buff) 
 
-        fname = buff.split(' /')[1]
-        fname = fname.split(' HTTP')[0]
-
-        print (fname)
-        
-        file_extension = fname.split('.')[1]
-
-        print(file_extension)
         try:
-            print("trying")
+            fname = buff.split('/')[1] #splits the HTTP request message and stores the name of the file requested with ' HTTP' attached to it
+            
+            if(fname == ' HTTP'): #if fname only has ' HTTP' that means no file was specified in the HTTP request header.
+                fname = 'index-1.html' 
+                file_extension = 'html'
+            else:
+                fname = fname.split(' HTTP')[0] #gets rid of the 'HTTP' that was next to the file name 
+                if(fname == 'favicon.ico'):
+                    continue
+                file_extension = fname.split('.')[1] #gets the file extension
 
-            file = open(fname, 'r') 
-            print("after opening") 
-            file_buff = file.read()  
-            print("after reading") 
+            
+
+            file = open(fname, 'rb') #opens the file to be read as binary 
+            file_buff = file.read()  #stores the file into a buffer
+           
             contentLength = str(len(file_buff)) 
 
             if (file_extension == 'html'):
@@ -54,46 +56,34 @@ if __name__ == "__main__":
             elif (file_extension == 'jpg'):
                 response_headers =  'Content-Type: image/jpg; encoding=utf8' 
             else:
-                print ('Invalid file type, we only support html and jpg!')
+                print ('Invalid file type, we only support html and jpg!') 
                 continue
-            print("before header")
-            header = 'HTTP/1.1 200 OK \nServer: SCU Web Server \n'
-            header = header + response_headers + '\nContent-Length: ' + contentLength + '\n\n'
 
-            header = bytes(header, encoding='utf-8')
-            file_buff = bytes(file_buff, encoding='utf-8')
-            print("before sending")
-            connection_fd.sendall(header)    
-            connection_fd.sendall(file_buff) 
+            header = 'HTTP/1.1 200 OK \nServer: SCU Web Server \n' + response_headers + '\nContent-Length: ' + contentLength + '\n\n'
+            print('HTTP Header Sent to Client: \n' + header)
+
+            header = bytes(header, encoding='utf-8') #converts the string header into a bytes object
+            header = header + file_buff 
+
+            connection_fd.sendall(header) 
             
-            print("before closing")
             file.close()
             connection_fd.close()         
             
         #Raised when an I/O operation (such as a print statement, the built-in open() function or a method of a file object) fails for an I/O-related reason, e.g., "file not found" or "disk full"
         except IOError:
-            # STUDENT WORK
+
             print('Error') 
 
-            file = open('error.html', 'r') 
+            file = open('error.html', 'rb') 
             file_buff = file.read()
-
             contentLength = str(len(file_buff))
-            header = 'HTTP/1.1 200 OK \nServer: SCU Web Server \nContent-Type: text/html \nContent-Length: ' + contentLength + '\n\n'
 
-            header = bytes(header, encoding='utf-8')
-            file_buff = bytes(file_buff, encoding='utf-8')
+            header = 'HTTP/1.1 200 OK \nServer: SCU Web Server \nContent-Type: text/html \nContent-Length: ' + contentLength + '\n\n'
+            header = bytes(header, encoding='utf-8') #converts the string header into bytes object 
+            header = header + file_buff 
 
             connection_fd.sendall(header) 
-            connection_fd.sendall(file_buff)
 
             file.close()
             connection_fd.close() 
-
-
-
-
-            
-
-
-# STUDENT WORK
