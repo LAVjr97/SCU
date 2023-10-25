9e227b3a8c2554848e93fd6cbd280acbbc933f36
LAVjr97 <lavjr97@gmail.com>
2023-10-24 21:58:19 -0700

    update 146 lab5

--- coen146/lab5/SMTPClient-local.py
+++ coen146/lab5/SMTPClient-local.py
@@ -21,7 +21,10 @@ mailserver = 'localhost'
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 # Port number may change according to the mail server
-clientSocket.connect(mailserver, 587)
+#like the bind funtion but instead takes the ip addr and port number as a tuple
+port = 6000
+addr = (mailserver, port)
+clientSocket.connect(addr)
 
 
 recv = clientSocket.recv(1024).decode()
@@ -31,38 +34,63 @@ if recv[:3] != '220':
 
 
 # Send HELO command along with the server address
-HELO = 'HELO lvillalta@scu.edu'
+HELO = 'HELO localhost:8080\r\n'
+HELO = bytes(HELO)
 
-sendall()
+clientSocket.sendall(HELO)
+recv = clientSocket.recv(1024).decode()
+print(recv)
 
 # Send MAIL FROM command and print server response
-# STUDENT WORK
+MAIL_FROM = "MAIL FROM: lvillalta@scu.edu\r\n"
+MAIL_FROM = bytes(MAIL_FROM)
 
+clientSocket.sendall(MAIL_FROM)
+recv = clientSocket.recv(1024).decode()
 
+print(recv)
 
 # Send RCPT TO command and print server response
 # STUDENT WORK
+RCPT_TO = 'RCPT TO: lvillalta@scu.edu\r\n'
+RCPT_TO = bytes(RCPT_TO)
 
-
+clientSocket.sendall(RCPT_TO)
+recv = clientSocket.recv(1024).decode()
+print(recv)
 
 # Send DATA command and print server response
 # STUDENT WORK
+DATA = 'DATA\r\n'
+DATA = bytes(DATA)
 
+clientSocket.sendall(DATA)
+recv = clientSocket.recv(1024).decode()
+print(recv)
 
-
-# Send message data.
-# STUDENT WORK
-
-
+# Send message data. 
+# STUDENT WORK 
+MSG_DATA = 'SUBJECT: Hello World\r\n Line 1\n Line 2\n Last Line\n'
+MSG_DATA = bytes(MSG_DATA)
 
 # Message to send
 # STUDENT WORK
-
+clientSocket.sendall(MSG_DATA)
 
 # Message ends with a single period
 # STUDENT WORK
+MSG_DATA = '\r\n.\r\n'
+MSG_DATA = bytes(MSG_DATA)
 
+clientSocket.sendall(MSG_DATA)
+recv = clientSocket.recv(1024).decode()
+print(recv)
 
 # Send QUIT command and get server response
 # STUDENT WORK
+QUIT = 'QUIT\r\n'
+QUIT = bytes(QUIT)
 
+clientSocket.sendall(QUIT)
+recv = clientSocket.recv(1024).decode()
+print(recv)
