db94672b4bf21c2da28cf0619603999d47ef55da
LAVjr97 <lavjr97@gmail.com>
2024-01-30 01:12:08 -0800

    update coen177

---
+++ coen177/lab3/lab3_4.c
@@ -0,0 +1,39 @@
+/**************************
+*   Lab3 - pipe()
+**************************/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <sys/wait.h> 
+
+// main
+int main(int argc,char *argv[]){
+   int  fds[2];
+   char buff[60];
+   int count;
+   int i;
+   pipe(fds);
+   
+   if (fork()==0){
+       printf("\nWriter on the upstream end of the pipe -> %d arguments \n",argc);
+       close(fds[0]);
+       dup2(fds[1], 1);
+       execlp("cat", "cat", "/etc/passwd", 0);
+       exit(0);
+   }
+   else if(fork()==0){
+       printf("\nReader on the downstream end of the pipe \n");
+       close(fds[1]);
+       execlp("grep", "grep", "root", NULL);
+
+       exit(0);
+    }
+   else{     
+      close(fds[0]);
+      close(fds[1]);
+      wait(0);
+      wait(0);
+   }
+return 0;
+}
