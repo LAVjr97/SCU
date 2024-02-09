7d11553bead3a4ef1aa6f59f9c20ae501f4e2947
LAVjr97 <lavjr97@gmail.com>
2024-02-09 03:03:01 -0800

    update coen177

--- coen177/lab4/lab4_3.c
+++ coen177/lab4/lab4_3.c
@@ -4,54 +4,67 @@
 #include <time.h>
 #include <pthread.h>
 
-#define N 5
-#define M 5
-#define L 4
+#define N 4
+#define M 3
+#define L 2
 
 double matrixA[N][M];
 double matrixB[M][L];
 double matrixC[N][L];
 
-void calculateMatrixC(int i){
-    for (int j = 0; j < L; j++){
-        double temp = 0;
-        for (int k = 0; k < M; k++){
+void *calculateMatrixC(void *t){
+    double temp; 
+    int i = (int)(intptr_t)t, j, k;
+
+    printf("\nThread's doing their work, %d\n", i);
+    for (j = 0; j < L; j++){
+        temp = 0;
+        for (k = 0; k < M; k++){
             temp += matrixA[i][k] * matrixB[k][j];
         }
+        printf("%d, %d\n", j, i);
         matrixC[i][j] = temp;
     }
+    return 0;
+}
+
+void printMatrix(int row, int col, double matrix[row][col]){
+    int i, j;
+    for(i = 0; i < row; i++){
+        for(j = 0; j < col; j++){
+                printf("%0.2f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+    return;
 }
 
 int main(){
     pthread_t thread_id[N];
-
     int i, j;
 
     //initializes the values for matrix A & B
     srand(time(NULL));
     for (i = 0; i < N; i++)
         for (j = 0; j < M; j++)
-            matrixA[i][j] = rand();
-
-    srand(time(NULL));
+            matrixA[i][j] = rand() % 10;
+        
     for (i = 0; i < M; i++)
         for (j = 0; j < L; j++)
-            matrixB[i][j] = rand();
+            matrixB[i][j] = rand() % 10; 
 
-    for(i = 0; i < N; i++){
-        pthread_create(&thread_id[N], NULL, &calculateMatrixC, i);
-    }
-
-    for(i = 0; i < N; i++){
-        pthread_join(thread_id[N], NULL);
-    }
 
     for(i = 0; i < N; i++)
-        for(j = 0; j < L; i++){
-            if(j + 1 == L)
-                printf("%f\n", matrixC[i][j]);
-            else
-                printf("%f ", matrixC[i][j]);
-        }
+        pthread_create(&thread_id[i], NULL, calculateMatrixC, (void*)(intptr_t)i);
+    
 
+    for(i = 0; i < N; i++)
+        pthread_join(thread_id[i], NULL);
+    
+
+    printf("All threads joined\n");
+    printMatrix(N, M, matrixA);
+    printMatrix(M, L, matrixB);
+    printMatrix(N, L, matrixC);
 }
