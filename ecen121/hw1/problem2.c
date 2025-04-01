2447446d4c24442557ebac98383cda2d71dcb118
LAVjr97 <130868019+LAVjr97@users.noreply.github.com>
2025-04-01 02:20:53 -0700

    ecen 121 hw1

--- ecen121/hw1/problem2.c
+++ ecen121/hw1/problem2.c
@@ -2,7 +2,6 @@
 
 void calc_date(unsigned long seconds);
 
-
 int main(void){
     unsigned long seconds;
     printf("Please enter the amount of seconds: ");
@@ -15,62 +14,31 @@ int main(void){
 
 /*Print out the month, day, year, and time.
     31,536,000 seconds in a year, 
-    604,800 seconds in a week
     86,400 in a day,
     3,600 in an hour
 */
+
 void calc_date(unsigned long inputSec){
-    int month = 0, day = 0, year = 0, hour = 0, minutes = 0, seconds = 0, startYear = 1949, weeks = 0, rem = 0;
+    int month = 0, day = 0, year = 0, hour = 0, minutes = 0, seconds = 0, startYear = 1949, rem = 0, i = 0;
+    int daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     year = inputSec / 31536000;
     rem = inputSec % 31536000;
     
-    
-    //weeks = rem / 604800;
-    //rem = rem % 604800;
-    
-    
+    //number of days in the year
     day = rem / 86400;
     rem = rem % 86400;
-    
-    switch(1){
-    	case(day <= 31):
-        	month = 1;
-            break;
-        case(day <= 59):
-        	month = 2;
-            break;
-        case(day <= 90):
-        	month = 3;
-            break;
-        case(day <= 120):
-        	month = 4;
-            break;
-        case(day <= 151):
-        	month = 5;
-            break;
-        case(day <= 181):
-        	month = 6;
-            break;
-        case(day <= 212):
-        	month = 7;
-            break;
-         case(day <= 243):
-         	month = 8;
-            break;
-        case(day <= 273):
-        	month = 9;
-            break;
-        case(day <= 304):
-        	month = 10;
-            break;
-        case(day <= 334):
-        	month 11;
-            break;
-        case(day <= 365):
-        	month 12;
-            break;
+
+    if(rem)
+        day++;
+
+    //day is compared to the number of days in a given month,
+    while(day >= daysInMonth[i]){
+        day -= daysInMonth[i];
+        i++;
     }
+
+    month = i + 1;
 
     hour = rem / 3600;
     rem = rem % 3600;
@@ -79,17 +47,13 @@ void calc_date(unsigned long inputSec){
     rem = rem % 60;
  	
     seconds = rem;
-    
-
-
-
-
-
-
-
 
     year += startYear;
 
-    printf("\n%d %d %d %d %d %d\n", year, month, day, hour, minutes, seconds);
-    //printf("\n%d/%d/%d %d:%d:%d", month, day, year, hour, minutes, seconds);
+    printf("\n%d/%d/%d %d:%d:%d", month, day, year, hour, minutes, seconds);
 }
+
+
+/*Please enter the amount of seconds: 652546067
+
+9/10/1969 14:47:47*/
---
+++ ecen121/hw1/problem3.c
@@ -0,0 +1,28 @@
+void sort(char **slist);
+
+int main(void){
+    char str1[] = "Hello";
+    char str2[] = "Dog";
+    char str3[] = "Cat";
+    char str4[] = "rabbit";
+    char str5[] = "man";
+    char str6[] = "woman";
+    char str7[] = "person";
+    char str8[] = "camera";
+    char str9[] = "TV";
+    char str10[] = "clueless";
+    char *slist[10] = {str1, str2, str3, str4, str5, str6, str7, str8, str9, str10};
+
+    sort(slist);
+}
+
+void sort(char **slist){
+    int i, j;
+
+    for(i = 0; i < 10; i++){
+        
+    }
+
+
+    for()
+}
